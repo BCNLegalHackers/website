30708a41df5ab7d4e32f595ae4db126074d087a3
Aldo Borrero <aldo@aldoborrero.com>
2020-04-01 16:30:55 +0200

    feat: added pulsating heart animation

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 import styles from './footer.module.scss'
@@ -20,9 +21,15 @@ const Footer = () => {
       </p>
       <p>
         Made with{' '}
-        <span className={styles.heartEmoji} role="img" aria-label="Heart">
+        <motion.span
+          animate={{ scale: [1, 1.2, 1, 1.2, 1] }}
+          transition={{ duration: 1.6, loop: Infinity }}
+          className={styles.heartEmoji}
+          role="img"
+          aria-label="Heart"
+        >
           ❤️
-        </span>{' '}
+        </motion.span>{' '}
         by <a href="https://41north.dev">°41North</a>
       </p>
     </footer>
